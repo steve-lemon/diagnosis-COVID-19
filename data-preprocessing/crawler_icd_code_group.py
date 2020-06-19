c0af1bb3a4a17db659f1643fb27228b99c733cdc
hspmuse <hspmuse@coupang.com>
2020-06-20 02:41:14 +0900

    icd_keyword mapping, symptom.

---
+++ data-preprocessing/crawler_icd_code_group.py
@@ -0,0 +1,55 @@
+﻿#-*- coding:utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+from bs4 import BeautifulSoup
+import re
+import ignore_kor_list_fuc as ignore
+from time import sleep
+
+chromedriver = './chromedriver'
+driver = webdriver.Chrome(chromedriver)
+
+depth1 = range(1, 23)
+depth2 = range(1, 30)
+
+icd_from = list()
+icd_to = list()
+icd_mid_name = list()
+
+for i in depth1:
+	url = "http://www.koicd.kr/2016/kcd/v7.do#" + str(i) + ".1&n"
+	driver.get(url)
+	sleep(0.5)
+	for j in depth2:
+		print(str(i) + "."+str(j))
+		html = driver.page_source
+		s1 = BeautifulSoup(html, 'html.parser')
+		id = str(i) + "." + str(j) + "_anchor"
+		try:
+			a1 = s1.find("a",{"id":id}).text
+			a2 = a1.split(" ")
+			print(a2[0])
+			print(len(a2[0]))
+			if len(a2[0]) == 3:
+				icd_from.append(a2[0])
+				icd_to.append(a2[0])
+				icd_mid_name.append(a1[4:])
+			else:
+				icd_from.append(a1[:3])
+				icd_to.append(a1[4:7])
+				icd_mid_name.append(a1[8:])
+		except AttributeError as ex:
+			continue
+
+d = {}
+d['icd_from'] = icd_from
+d['icd_to'] = icd_to
+d['icd_group_name'] = icd_mid_name
+
+icd_dict = pd.DataFrame(d)
+
+print(icd_dict)
+icd_dict.to_csv("./data2/icd_group.csv", index=True, encoding="UTF-8")
---
+++ data-preprocessing/crawler_keyword_symptom.py
@@ -0,0 +1,62 @@
+﻿#-*- coding:utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+from bs4 import BeautifulSoup
+import re
+import ignore_kor_list_fuc as ignore
+
+merge_data = pd.read_csv('./data2/my-icd-major-patients.csv')
+icd_keyword = pd.read_csv('./data2/icd_keyword_mapping.csv')
+
+chromedriver = './chromedriver'
+driver = webdriver.Chrome(chromedriver)
+
+symptomList = []
+
+for index, row in merge_data.iterrows():
+	a = icd_keyword[icd_keyword['icd_code'] == row['icd_code']]
+	keyword = a.get_value(a.index[0], 'keyword')
+	if str(keyword) == "nan":
+		symptomList.append("Empty Icd Name")
+		continue
+
+	symptomSet = set()
+
+	url = "http://www.amc.seoul.kr/asan/healthinfo/disease/diseaseList.do?searchKeyword=" + keyword
+	driver.get(url)
+	try:
+		wait = WebDriverWait(driver, 10)
+		element = wait.until(EC.element_to_be_clickable((By.ID, 'footerWrap')))
+	except:
+		driver.get(url)
+		wait = WebDriverWait(driver, 10)
+		element = wait.until(EC.element_to_be_clickable((By.ID, 'footerWrap')))
+
+	html = driver.page_source
+	soup = BeautifulSoup(html, 'html.parser')
+	try:
+		a_tag_list = soup.findAll("a", href=re.compile("symptomId"))
+		print(a_tag_list)
+
+		# 한번이라도 검색 결과가 있으면 return!!!
+		for n in a_tag_list:
+			print(n.text.strip())
+			symptomSet.add(n.text.strip())
+
+	except Exception as ex:
+		print(ex)
+		# symptomList.append("error_data")
+	print("-----------------------------")
+	print(symptomSet)
+	print("-----------------------------")
+	symptomList.append('||'.join(symptomSet))
+
+
+merge_data['symptom'] = symptomList
+
+# data['inclusion'] = inclusion
+# data['exclusion'] = exclusion
+merge_data.to_csv('./data2/my-icd-major-patients-symptom.csv', index=True, encoding="UTF-8")
