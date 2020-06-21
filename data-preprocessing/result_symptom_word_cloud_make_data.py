635697f380e1cf065e60e07d3b062521569cbe23
hspmuse <hspmuse@coupang.com>
2020-06-21 18:15:03 +0900

    symptom count

---
+++ data-preprocessing/result_symptom_word_cloud_make_data.py
@@ -0,0 +1,54 @@
+﻿#-*- coding:utf-8 -*-
+import pandas as pd
+
+from collections import Counter
+import common_fuc
+
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+import matplotlib
+from matplotlib import rc
+
+rc('font', family='NanumBarunGothic')
+
+merge_data = pd.read_csv('./data2/my-icd-major-patients-symptom.csv')
+symptom_list = []
+
+isWeight = True
+
+for index, row in merge_data.iterrows():
+	# print(row['symptom'])
+	row_symptom_list = str(row['symptom']).split("||")
+	weight = common_fuc.getWeight(row['sum'], isWeight)
+	print("weight=" + str(weight))
+	for word in row_symptom_list:
+		if common_fuc.isIgnoreSymptom(str(word)):
+			continue
+		num = 1
+		while num <= weight:
+			print(num)
+			symptom_list.append(word)
+			num = num+1
+
+print(len(symptom_list))
+
+counts = Counter(symptom_list)
+tags = counts.most_common(1000)
+
+top20 = pd.DataFrame(tags)
+
+if isWeight:
+	top20.to_csv('./result/symptom_result_weight.csv', index=True, encoding="UTF-8")
+else:
+	top20.to_csv('./result/symptom_result.csv', index=True, encoding="UTF-8")
+
+
+# WordCloud 한글 깨짐. 현상 발생. 주석처리.
+# wc = WordCloud(font_path='/Library/Fonts/NanumBarunGothic.ttf', background_color='white', width=800, height=600)
+# cloud = wc.generate_from_frequencies(dict(tags))
+# plt.figure(figsize=(10, 8))
+# plt.axis('off')
+# plt.imshow(cloud)
+# plt.show()
+
